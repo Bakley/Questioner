ddb44c7122e1345312c88e60df1822736857eb6f
Bakley <barclaykoin@gmail.com>
2019-01-07 09:39:16 +0300

    [feature #163013797] add the modular for each button

---
+++ UI/main.js
@@ -0,0 +1,26 @@
+// sign in modular
+document.getElementById('signin').addEventListener('click', () => {
+  document.querySelector('.bg-modal2').style.display = 'flex';
+});
+
+document.querySelector('.close').addEventListener('click', () => {
+  document.querySelector('.bg-modal2').style.display = 'none';
+});
+
+// sigh up modular
+document.getElementById('signup').addEventListener('click', () => {
+  document.querySelector('.bg-modal2').style.display = 'flex';
+});
+
+document.querySelector('.close').addEventListener('click', () => {
+  document.querySelector('.bg-modal2').style.display = 'none';
+});
+
+// Admin sign in modular
+document.getElementById('admin').addEventListener('click', () => {
+  document.querySelector('.bg-modal2').style.display = 'flex';
+});
+
+document.querySelector('.close').addEventListener('click', () => {
+  document.querySelector('.bg-modal2').style.display = 'none';
+});
